0e2dd6ac6e10c8a72cbe4561e13f7993a24e118b
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-06-22 19:58:38 +0100

    chore: more more;

---
+++ 1.haspathsum.go
@@ -0,0 +1,27 @@
+package main
+ 
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+    var pathSum func(a *TreeNode, sum int) bool
+
+    pathSum = func(a *TreeNode, sum int) bool {
+        if a == nil {
+            return false
+        }
+
+        sum += a.Val
+
+        if a.Left == nil && a.Right == nil {
+            return sum == targetSum
+        }
+
+        return pathSum(a.Left, sum) || pathSum(a.Right, sum)
+    }
+
+    return pathSum(root, 0)
+}
