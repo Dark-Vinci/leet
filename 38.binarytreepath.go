9b255292f6f779e00fa12dc3de342747a7147fc5
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-07-11 23:03:07 +0100

    chore: core dump

---
+++ 38.binarytreepath.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func binaryTreePaths(root *TreeNode) []string {
+    if root == nil {
+        return []string{}
+    }
+
+    result := make([]string, 0)
+
+    var dfs func(a *TreeNode, path string, start bool)
+
+    dfs = func(a *TreeNode, path string, start bool) {
+        if a == nil {
+            return
+        }
+
+        if start {
+            path = fmt.Sprintf("%v", a.Val)
+        } else {
+            path += fmt.Sprintf("->%v", a.Val)
+        }
+
+        if a.Left == nil && a.Right == nil {
+            result = append(result, path)
+            return
+        }
+
+        dfs(a.Left, path, false)
+        dfs(a.Right, path, false)
+    }
+
+    dfs(root, "", true)
+
+    return result
+}
---
+++ 40.sumroottoleafnumber.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func sumNumbers(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+
+    db := make([]string, 0)
+
+    var dfs func(node *TreeNode, values string)
+
+    dfs = func(node *TreeNode, values string) {
+        if node == nil {
+            return
+        }
+
+        values += fmt.Sprintf("%v", node.Val)
+
+        if node.Left == nil && node.Right == nil {
+            db = append(db, values)
+            return
+        }
+
+        dfs(node.Left, values)
+        dfs(node.Right, values)
+    }
+
+    dfs(root, "")
+
+    sum := 0
+
+    for _, j := range db {
+        jj, _ := strconv.Atoi(j)
+
+        sum += jj
+    }
+
+    return sum
+}
