e33d957c3fd56942c094613efb4b5ac7c430c501
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-07-14 20:50:47 +0100

    chore: more work

---
+++ 47.mergesort.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+func quickSort(arr []int) []int {
+	
+	var recurse func(a []int) [] int
+	
+	recurse = func(a []int) []int {
+		if len(a) <= 1 {
+			return a
+		}
+		
+		pivot := a[len(a) - 1]
+		
+		left := finds(a, pivot, true)
+		
+		sortedLeft := recurse(left)
+		
+		right := finds(a, pivot, false)
+		
+		sortedRight := recurse(right)
+		
+		res := make([]int, 0)
+		
+		res = append(res, sortedLeft...)
+		res = append(res, pivot)
+		res = append(res, sortedRight...)
+		
+		return res
+	}
+	
+	return recurse(arr)
+}
+
+func finds(arr []int, pivot int, min bool) []int {
+	result := make([]int, 0)
+	
+	for _, j := range arr {
+		if min && j < pivot {
+			result = append(result, j)
+		} else if !min && j > pivot {
+			result = append(result, j)
+		}
+	}
+	
+	
+	return result
+}
+
+func main() {
+	arr := []int {100, 3, 7, 0, 1000, 556728, 7,88}
+	
+	res := quickSort(arr)
+	
+	fmt.Println(res)
+}
