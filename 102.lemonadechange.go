401a2c61fa116b681a6542c9e4bf362e354f3ffe
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-08-22 23:07:52 +0100

    chore; clearly theres mpre

---
+++ 102.lemonadechange.go
@@ -0,0 +1,39 @@
+package main
+
+func lemonadeChange(bills []int) bool {
+	five, ten := 0, 0
+
+	for i := 0; i < len(bills); i++ {
+		if bills[i] == 5 {
+			five++
+			continue
+		}
+
+		change := bills[i] - 5
+		if change == 5 {
+			if five <= 0 {
+				return false
+			}
+
+			five--
+			ten++
+		}
+
+		if change == 15 {
+			if (five*5 + ten*10) < 15 {
+				return false
+			}
+
+			if ten > 0 && five > 0 {
+				ten--
+				five--
+			} else if five >= 3 {
+				five = five - 3
+			} else {
+				return false
+			}
+		}
+	}
+
+	return true
+}
---
+++ 102.teemoattacking.go
@@ -0,0 +1,22 @@
+package main
+
+func findPoisonedDuration(timeSeries []int, duration int) int {
+	count := 0
+
+	for i := 0; i < len(timeSeries); i++ {
+		dur := timeSeries[i] + duration - 1
+
+		if i == len(timeSeries)-1 {
+			count += duration
+			continue
+		}
+
+		if dur < timeSeries[i+1] {
+			count += duration
+		} else {
+			count += timeSeries[i+1] - timeSeries[i]
+		}
+	}
+
+	return count
+}
