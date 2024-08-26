827989d7a55c3caaf674ca6a4fc17a712a85e80c
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-08-26 22:14:16 +0100

    chore: todays algo

---
+++ 117.convertbst.go
@@ -0,0 +1,25 @@
+package main
+
+func convertBST(root *TreeNode) *TreeNode {
+	var dfs func(r *TreeNode, top int) int
+
+	dfs = func(r *TreeNode, top int) int {
+		if r == nil {
+			return 0
+		}
+
+		right := dfs(r.Right, top)
+
+		curr := r.Val
+
+		r.Val += right + top
+
+		left := dfs(r.Left, r.Val)
+
+		return curr + left + right
+	}
+
+	dfs(root, 0)
+
+	return root
+}
