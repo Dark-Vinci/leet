2cac5dbfced485097d2178d0159076acb9751335
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-10-20 05:45:08 +0100

    chore; more cleaity

---
+++ 167.BINARYTREECOUSINE.go
@@ -0,0 +1,38 @@
+package main
+
+func isCousins(root *TreeNode, x int, y int) bool {
+	type node struct {
+		par  *TreeNode
+		curr *TreeNode
+	}
+
+	q := []*node{&node{par: nil, curr: root}}
+
+	for len(q) > 0 {
+		l := len(q)
+		var first *node
+
+		for i := 0; i < l; i++ {
+			c := q[0]
+			q = q[1:]
+
+			if c.curr.Left != nil {
+				q = append(q, &node{par: c.curr, curr: c.curr.Left})
+			}
+
+			if c.curr.Right != nil {
+				q = append(q, &node{par: c.curr, curr: c.curr.Right})
+			}
+
+			if c.curr.Val == x || c.curr.Val == y {
+				if first == nil {
+					first = c
+				} else if first.par != c.par {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
+}
