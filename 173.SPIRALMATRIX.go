b8b777dc4b8d2363d1a7463e672311bc5840be76
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-10-22 06:46:37 +0100

    chore: more to come

---
+++ 173.SPIRALMATRIX.go
@@ -0,0 +1,46 @@
+package main
+
+func generateMatrix(n int) [][]int {
+	var (
+		left, right = 0, n - 1
+		top, bot    = 0, n - 1
+		val         = 1
+		result      = make([][]int, n)
+	)
+
+	for j := 0; j < n; j++ {
+		result[j] = make([]int, n)
+	}
+
+	for left <= right {
+		// left to right
+		for i := left; i < right+1; i++ {
+			result[top][i] = val
+			val++
+		}
+		top++
+
+		// top to down
+		for i := top; i < bot+1; i++ {
+			result[i][right] = val
+			val++
+		}
+		right--
+
+		// left to right
+		for i := right; i >= left; i-- {
+			result[bot][i] = val
+			val++
+		}
+		bot--
+
+		// down to up
+		for i := bot; i >= top; i-- {
+			result[i][left] = val
+			val++
+		}
+		left++
+	}
+
+	return result
+}
