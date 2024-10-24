fbf4507edc8a06d0901aff2101c8ef0181915bfb
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-10-24 22:37:06 +0100

    chore: more worl

---
+++ 178.TRANSPOSEMATRIX.go
@@ -0,0 +1,17 @@
+package main
+
+func transpose(matrix [][]int) [][]int {
+	result := make([][]int, len(matrix[0]))
+
+	for i := 0; i < len(result); i++ {
+		result[i] = make([]int, len(matrix))
+	}
+
+	for j := 0; j < len(matrix); j++ {
+		for i := 0; i < len(matrix[0]); i++ {
+			result[i][j] = matrix[j][i]
+		}
+	}
+
+	return result
+}
