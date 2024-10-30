c247ad665ace06912cc6d0b3a7ffd414480b1817
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-10-30 18:31:43 +0000

    chore: more like this

---
+++ 189.JUMPGAME.go
@@ -0,0 +1,14 @@
+package main
+
+func canJump(nums []int) bool {
+	l := len(nums)
+	last := l - 1
+
+	for i := l - 2; i >= 0; i-- {
+		if i+nums[i] >= last {
+			last = i
+		}
+	}
+
+	return last == 0
+}
