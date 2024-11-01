d5f2e42d6846b63d4efe3cbb0dc69f9ac6221766
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-11-01 14:46:11 +0000

    chore: more update

---
+++ 193.DIFFWAYTOADDPARENTHESIS.go
@@ -0,0 +1,47 @@
+package main
+
+import "strconv"
+
+func diffWaysToCompute(expression string) []int {
+	var (
+		dfs  func(exp string) []int
+		memo = make(map[string][]int)
+	)
+
+	dfs = func(exp string) []int {
+		if v, ok := memo[exp]; ok {
+			return v
+		}
+
+		if len(exp) <= 2 {
+			v, _ := strconv.Atoi(exp)
+			return []int{v}
+		}
+
+		ans := make([]int, 0)
+
+		for i, v := range exp {
+			if v == '+' || v == '-' || v == '*' {
+				left, right := dfs(exp[:i]), dfs(exp[i+1:])
+
+				for _, l := range left {
+					for _, r := range right {
+						if v == '+' {
+							ans = append(ans, l+r)
+						} else if v == '-' {
+							ans = append(ans, l-r)
+						} else {
+							ans = append(ans, l*r)
+						}
+					}
+				}
+			}
+		}
+
+		memo[exp] = ans
+
+		return ans
+	}
+
+	return dfs(expression)
+}
---
+++ 194.DECODESTRING.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func decodeString(s string) string {
+	multiple := make([]int, 0)
+	str := make([]string, 0)
+	curr, num := "", ""
+
+	for i := 0; i < len(s); i++ {
+		e := string(s[i])
+
+		if _, err := strconv.Atoi(e); err == nil {
+			num += e
+			continue
+		}
+
+		if e == "[" {
+			m, _ := strconv.Atoi(num)
+			multiple, str = append(multiple, m), append(str, curr)
+
+			curr, num = "", ""
+		} else if e == "]" {
+			curr = str[len(str)-1] + (strings.Repeat(curr, multiple[len(multiple)-1]))
+
+			str, multiple = str[:len(str)-1], multiple[:len(multiple)-1]
+		} else {
+			curr += e
+		}
+	}
+
+	return curr
+}
