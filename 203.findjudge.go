71aa3d45f4b33ca8969bed4f4b99ec3800691a73
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-12-13 22:39:50 +0000

    chore; morely

--- 203.findjudge.go
+++ 203.findjudge.go
@@ -6,50 +6,55 @@ func findJudge(n int, trust [][]int) int {
 	if len(trust) == 0 && n == 1 {
 		return n
 	}
-
 	list := toAdj(trust)
 	res, count := -1, 0
-
 	for key, val := range list {
 		if len(val) == 0 {
 			res = key
 			break
 		}
 	}
-
 	if res < 0 {
 		return res
 	}
-
 	for _, val := range list {
 		if slices.Contains(val, res) {
 			count++
 		}
 	}
-
 	if count == n-1 {
 		return res
 	}
-
 	return -1
 }
-
 func toAdj(trust [][]int) map[int][]int {
 	result := make(map[int][]int)
-
 	for _, tr := range trust {
 		u, v := tr[0], tr[1] // trustee, trusted
-
 		if _, ok := result[u]; !ok {
 			result[u] = make([]int, 0)
 		}
-
 		if _, ok := result[v]; !ok {
 			result[v] = make([]int, 0)
 		}
-
 		result[u] = append(result[u], v)
 	}
-
 	return result
+}
+
+func findJudgeBETTER_SOLUTION(n int, trust [][]int) int {
+	inDegree := make(map[int]int)
+	outDegree := make(map[int]int)
+
+	for _, t := range trust {
+		inDegree[t[1]]++
+		outDegree[t[0]]++
+	}
+
+	for i := 1; i <= n; i++ {
+		if inDegree[i] == n-1 && outDegree[i] == 0 {
+			return i
+		}
+	}
+	return -1
 }
