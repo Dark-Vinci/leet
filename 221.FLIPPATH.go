1832d69b32e4c8b8332b637efd4084c344390142
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-01-08 17:41:45 +0000

    chore; flip path

---
+++ 221.FLIPPATH.go
@@ -0,0 +1,35 @@
+package main
+
+func flipMatchVoyage(root *TreeNode, voyage []int) []int {
+	var (
+		dfs    func(r *TreeNode) bool
+		result = make([]int, 0)
+		index  int
+	)
+
+	dfs = func(r *TreeNode) bool {
+		if r == nil {
+			return true
+		}
+
+		if r.Val != voyage[index] {
+			return false
+		}
+
+		index++
+
+		if r.Left != nil && r.Left.Val != voyage[index] {
+			result = append(result, r.Val)
+
+			return dfs(r.Right) && dfs(r.Left)
+		}
+
+		return dfs(r.Left) && dfs(r.Right)
+	}
+
+	if !dfs(root) {
+		return []int{-1}
+	}
+
+	return result
+}
