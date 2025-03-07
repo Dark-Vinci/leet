0095e10309f774d8ca8f3d4b376f0eaa6ad48f56
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-03-07 19:14:38 +0000

    chore; more headaches

---
+++ 245.MAX_POINT.1423.go
@@ -0,0 +1,16 @@
+package main
+
+import "math"
+
+func maxCardScore(cardPoints []int, k int) int {
+	mx := math.MinInt
+
+	for i := k; i >= 0; i-- {
+		rIndex := len(cardPoints) - k + i
+		l, r := sum(cardPoints[0:i]), sum(cardPoints[rIndex:len(cardPoints)])
+
+		mx = max(mx, l+r)
+	}
+
+	return mx
+}
