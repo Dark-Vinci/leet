e87fcfa569102f6f7de239421f082931ef5fc520
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-03-30 17:12:49 +0100

    chore; closer

---
+++ 267.MAX_CONTAINER_ON_SHIP.3492.go
@@ -0,0 +1,19 @@
+package main
+
+func maxContainers(n int, w int, maxWeight int) int {
+	var (
+		result        = 0
+		cell          = n * n
+		currentWeight = 0
+	)
+
+	for i := 0; i < cell; i++ {
+		currentWeight += w
+
+		if currentWeight <= maxWeight {
+			result++
+		}
+	}
+
+	return result
+}
