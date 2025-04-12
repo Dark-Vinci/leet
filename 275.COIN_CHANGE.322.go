3fb22af9518dd6f915ff0cc9a5b4e05c4af07ba5
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-04-12 17:00:17 +0100

    chore; one for today

---
+++ 275.COIN_CHANGE.322.go
@@ -0,0 +1,31 @@
+package main
+
+import "math"
+
+func coinChange1(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+
+	for i := 0; i < len(dp); i++ {
+		dp[i] = math.MaxInt
+	}
+
+	dp[0] = 0
+
+	for i := 0; i < len(dp); i++ {
+		if dp[i] == math.MaxInt {
+			continue
+		}
+
+		for _, op := range coins {
+			if i+op < len(dp) {
+				dp[i+op] = min(dp[i]+1, dp[i+op])
+			}
+		}
+	}
+
+	if dp[amount] == math.MaxInt {
+		return -1
+	}
+
+	return dp[amount]
+}
