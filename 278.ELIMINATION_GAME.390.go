1cffe7890865ef62e60ac43098c31034b8d18d4f
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-06-13 01:15:35 +0100

    chore; format code

--- 278.ELIMINATION_GAME.390.go
+++ 278.ELIMINATION_GAME.390.go
@@ -1,59 +1,61 @@
 package main
 
-func lastRemaining(n int) int {
-    if n == 1 {
-        return n
-    }
+import "slices"
 
-    return 2 * (1 + (n/2) - lastRemaining(n/2))
+func lastRemaining(n int) int {
+	if n == 1 {
+		return n
+	}
+
+	return 2 * (1 + (n / 2) - lastRemaining(n/2))
 }
 
-//  exceed time limit
+// exceed time limit
 func lastRemaining_(n int) int {
-    var (
-        helper func(arr []int, forw bool)
-        arr = make([]int, 0)
-        result = -1
-    )
+	var (
+		helper func(arr []int, forw bool)
+		arr    = make([]int, 0)
+		result = -1
+	)
 
-    for i := 1; i <= n; i++ {
-        arr = append(arr, i)
-    }
+	for i := 1; i <= n; i++ {
+		arr = append(arr, i)
+	}
 
-    helper = func(arr []int, forw bool) {
-        if len(arr) == 1 {
-            result = arr[0]
-            return
-        }
+	helper = func(arr []int, forw bool) {
+		if len(arr) == 1 {
+			result = arr[0]
+			return
+		}
 
-        nn := make([]int, 0)
+		nn := make([]int, 0)
 
-        ll := len(arr) - 1
+		ll := len(arr) - 1
 
-        for i := 0; i < len(arr); i++ {
-            if forw {
-                if i % 2 != 0 {
-                    nn = slices.Insert(nn, len(nn), arr[i])
-                }
-            } else {
-                j := len(arr) - i - 1
+		for i := 0; i < len(arr); i++ {
+			if forw {
+				if i%2 != 0 {
+					nn = slices.Insert(nn, len(nn), arr[i])
+				}
+			} else {
+				j := len(arr) - i - 1
 
-                if ll % 2 == 0 {
-                    if j % 2 != 0 {
-                        nn = slices.Insert(nn, 0, arr[j])
-                    }
-                } else {
-                    if j % 2 == 0 {
-                        nn = slices.Insert(nn, 0, arr[j])
-                    }
-                }
-            }
-        }
+				if ll%2 == 0 {
+					if j%2 != 0 {
+						nn = slices.Insert(nn, 0, arr[j])
+					}
+				} else {
+					if j%2 == 0 {
+						nn = slices.Insert(nn, 0, arr[j])
+					}
+				}
+			}
+		}
 
-        helper(nn, !forw)
-    }
+		helper(nn, !forw)
+	}
 
-    helper(arr, true)
+	helper(arr, true)
 
-    return result
+	return result
 }
--- dynamicProgramming/14.MIN_SAFE.2812.go
+++ dynamicProgramming/14.MIN_SAFE.2812.go
@@ -4,7 +4,7 @@ import "container/heap"
 
 type Jtem struct {
 	dist int
-	i, j   int
+	i, j int
 }
 
 type MaxHeap []Jtem
@@ -34,102 +34,101 @@ func (pq *MaxHeap) Pop() any {
 	return last
 }
 
-
 func maximumSafenessFactor(grid [][]int) int {
-    var (
-        l = len(grid)
-        prelim func() map[[2]int] int
-    )
+	var (
+		l      = len(grid)
+		prelim func() map[[2]int]int
+	)
 
-    prelim = func() map[[2]int] int {
-        var (
-	       	q = [][3]int{}
-	        distMap = make(map[[2]int] int)
-        )
+	prelim = func() map[[2]int]int {
+		var (
+			q       = [][3]int{}
+			distMap = make(map[[2]int]int)
+		)
 
-        for i := range l {
-            for j := range l {
-                if grid[i][j] == 1 {
-                    q = append(q, [3]int{i, j, 0})
-                    distMap[[2]int{i, j}] = 0
-                }
-            }
-        }
+		for i := range l {
+			for j := range l {
+				if grid[i][j] == 1 {
+					q = append(q, [3]int{i, j, 0})
+					distMap[[2]int{i, j}] = 0
+				}
+			}
+		}
 
-        for len(q) > 0 {
-            ll := len(q)
+		for len(q) > 0 {
+			ll := len(q)
 
-            for i := 0; i < ll; i++ {
-                curr := q[0]
-                q = q[1:]
+			for i := 0; i < ll; i++ {
+				curr := q[0]
+				q = q[1:]
 
-                ii, jj, dist := curr[0], curr[1], curr[2]
+				ii, jj, dist := curr[0], curr[1], curr[2]
 
-                dirs := [][]int {
-                    {ii, jj + 1},
-                    {ii, jj - 1},
-                    {ii + 1, jj},
-                    {ii - 1, jj},
-                }
+				dirs := [][]int{
+					{ii, jj + 1},
+					{ii, jj - 1},
+					{ii + 1, jj},
+					{ii - 1, jj},
+				}
 
-                for _, val := range dirs {
-                    x, y := val[0], val[1]
-                    pos := [2]int{x, y}
+				for _, val := range dirs {
+					x, y := val[0], val[1]
+					pos := [2]int{x, y}
 
-                    if x >= 0 && x < l && y >= 0 && y < l {
-                        if _, ok := distMap[pos]; !ok {
-                            q = append(q, [3]int{x, y, dist + 1})
-                            distMap[[2]int{x, y}] = dist + 1
-                        }
-                    }
-                }
-            }
-        }
+					if x >= 0 && x < l && y >= 0 && y < l {
+						if _, ok := distMap[pos]; !ok {
+							q = append(q, [3]int{x, y, dist + 1})
+							distMap[[2]int{x, y}] = dist + 1
+						}
+					}
+				}
+			}
+		}
 
-        return distMap
-    }
+		return distMap
+	}
 
-    var (
-        mp = prelim()
-        mh = make(MaxHeap, 0)
-        visited = make(map[[2]int]struct{})
-    )
+	var (
+		mp      = prelim()
+		mh      = make(MaxHeap, 0)
+		visited = make(map[[2]int]struct{})
+	)
 
-    heap.Init(&mh)
-    heap.Push(&mh, Jtem{i: 0, j: 0, dist: mp[[2]int{0, 0}]})
+	heap.Init(&mh)
+	heap.Push(&mh, Jtem{i: 0, j: 0, dist: mp[[2]int{0, 0}]})
 
-    for mh.Len() > 0 {
-        curr := heap.Pop(&mh).(Jtem)
+	for mh.Len() > 0 {
+		curr := heap.Pop(&mh).(Jtem)
 
-        dist, i, j := curr.dist, curr.i, curr.j
+		dist, i, j := curr.dist, curr.i, curr.j
 
-        if i == l - 1 && j == l - 1 {
-            return dist
-        }
+		if i == l-1 && j == l-1 {
+			return dist
+		}
 
-        dirs := [][]int {
-            {i, j + 1},
-            {i, j - 1},
-            {i + 1, j},
-            {i - 1, j},
-        }
+		dirs := [][]int{
+			{i, j + 1},
+			{i, j - 1},
+			{i + 1, j},
+			{i - 1, j},
+		}
 
-        for _, val := range dirs {
-            x, y := val[0], val[1]
+		for _, val := range dirs {
+			x, y := val[0], val[1]
 
-            if x >= 0 && y >= 0 && x < l && y < l {
-                pos := [2]int{x, y}
+			if x >= 0 && y >= 0 && x < l && y < l {
+				pos := [2]int{x, y}
 
-                if _, ok := visited[pos]; !ok {
-                    visited[pos] = struct{}{}
+				if _, ok := visited[pos]; !ok {
+					visited[pos] = struct{}{}
 
-                    newDist := min(dist, mp[pos])
+					newDist := min(dist, mp[pos])
 
-                    heap.Push(&mh, Jtem{i: x, j: y, dist: newDist})
-                }
-            }
-        }
-    }
+					heap.Push(&mh, Jtem{i: x, j: y, dist: newDist})
+				}
+			}
+		}
+	}
 
-    return -1
+	return -1
 }
