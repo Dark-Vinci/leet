dd53893a106aa46f6a984578905aa29cf8abd097
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2025-06-13 21:12:19 +0100

    chore; first two

---
+++ 282.MAX_CONSECUTIVE_ONES.go
@@ -0,0 +1,17 @@
+package main
+
+func findMaxConsecutiveOnes(nums []int) int {
+	result, count := 0, 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			result = max(count, result)
+			count = 0
+			continue
+		}
+
+		count++
+	}
+
+	return max(result, count)
+}
---
+++ 283.CONSECUTIVE_CHARACTER.go
@@ -0,0 +1,17 @@
+package main
+
+func maxPower(s string) int {
+	result, count := 1, 1
+
+	for i := 1; i < len(s); i++ {
+		if s[i-1] != s[i] {
+			result = max(result, count)
+			count = 1
+			continue
+		}
+
+		count++
+	}
+
+	return max(result, count)
+}
